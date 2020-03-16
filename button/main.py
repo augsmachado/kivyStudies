e30c9e8ad22aa7cad25c22dc3edacfd288b7f8c6
Augs Machado <augustomachadoramos@gmail.com>
2020-03-16 10:28:35 -0300

    Add box2

--- button/main.py
+++ button/main.py
@@ -1,9 +1,29 @@
 from kivy.app import App
 from kivy.uix.button import Button
-
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
 class Test(App):
     def build(self):
-        return Button(text='olá mundo')
+        # empilha widgets como caixas
+        box = BoxLayout(orientation="vertical")
+        
+        button = Button(text='Botao 1')
+        label = Label(text='Texto 1')
+        
+        box.add_widget(button)
+        box.add_widget(label)
+
+
+    	# combinacao de layout
+        box2 = BoxLayout()
+        
+        button2 = Button(text='Botao 2')
+        label2 = Label(text='Texto 2')
+        
+        box2.add_widget(button2)
+        box2.add_widget(label2)
+        box.add_widget(box2)
+        return box
 
 Test().run()
